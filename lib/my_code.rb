7cba7264493f1db09908d508599926c7530ce041
Sara Fotros <sarafotros1@yahoo.com>
2019-11-14 20:55:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,10 @@
 # My Code here....
-def map_to_negativize([1, 2, 3, -9])
-  array
+def map_to_negativize(array)
+  array = [1, 2, 3, -9]
+  negative_arr = []
+   i = 0 
+   while i < array.size do
+     negative_arr= array[i] * -1
+     i += -1
+   end
+  negative_arr 
